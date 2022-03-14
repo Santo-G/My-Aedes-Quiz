f8897ceccc10307685fc82956c22c3deeb7f67b7
Santo-G <santogulletta91@gmail.com>
2022-03-14 19:20:50 +0100

    feat: first layout structure implementation

---
+++ app/src/main/java/com/androidapp/myaedesquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidapp.myaedesquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
